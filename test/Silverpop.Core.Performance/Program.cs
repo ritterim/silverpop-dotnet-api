77a042c7fe334ea0d33172b60074eec945bcd88c
Ken Dale <ken@kendaleiv.com>
2014-12-04 16:39:09 -0500

    Add performance test for TransactMessage GetRecipientBatchedMessages

---
+++ test/Silverpop.Core.Performance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Silverpop.Core.Performance
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var numberOfTags = 15;
+            var tagValue = new string(
+                Enumerable.Repeat("ABC", 1000)
+                    .SelectMany(x => x)
+                    .ToArray());
+
+            var personalizationTags = new Dictionary<string, string>();
+
+            for (var i = 0; i < numberOfTags; i++)
+                personalizationTags.Add("Tag" + i, tagValue);
+
+            Console.WriteLine("Testing 1 million recipients with {0} tags using batches of 5000:", numberOfTags);
+            new TransactMessagePerformance()
+                .InvokeGetRecipientBatchedMessages(1000000, 5000, personalizationTags);
+
+            Console.WriteLine("Testing 10 million recipients with {0} tags using batches of 5000:", numberOfTags);
+            new TransactMessagePerformance()
+                .InvokeGetRecipientBatchedMessages(10000000, 5000, personalizationTags);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test/Silverpop.Core.Performance/TransactMessagePerformance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Silverpop.Core.Performance
+{
+    public class TransactMessagePerformance
+    {
+        public void InvokeGetRecipientBatchedMessages(
+            int recipientCount,
+            int recipientsPerBatch,
+            IDictionary<string, string> personalizationTagsForEachRecipient)
+        {
+            var message = new TransactMessage()
+            {
+                Recipients = Enumerable.Range(0, recipientCount)
+                    .Select(x => new TransactMessageRecipient()
+                    {
+                        EmailAddress = Guid.NewGuid().ToString() + "@example.com",
+                        PersonalizationTags = personalizationTagsForEachRecipient
+                    })
+                    .ToList()
+            };
+
+            var stopwatch = Stopwatch.StartNew();
+            Console.WriteLine("Stopwatch started.");
+
+            Console.WriteLine("Calling GetRecipientBatchedMessages on constructed message...");
+            var recipientBatchedMesssages = message.GetRecipientBatchedMessages(recipientsPerBatch);
+
+            Console.WriteLine("{0} messages were generated.", recipientBatchedMesssages.Count());
+            Console.WriteLine();
+
+            stopwatch.Stop();
+            Console.WriteLine("Operation took {0}", stopwatch.Elapsed.ToString());
+            Console.WriteLine();
+        }
+    }
+}
